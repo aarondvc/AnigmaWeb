2389aa50431bc6317476ef8a0013581634f294a9
aarondvc <adv69240@uga.edu>
2025-09-03 18:51:47 -0400

    Added AboutSection component and fixed HeroSection

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,11 @@
+export const AboutSection = () => {
+ return (
+   <section id="about" className="py-24 px-4 relative">
+      <div className="container max-w-4xl mx-auto text-center">
+         <h2 className="text-3xl md:text-4xl font-bold text-foreground mb-12">
+            About <span className="text-primary">Me</span>
+         </h2>
+      </div>
+   </section>
+ );
+};
