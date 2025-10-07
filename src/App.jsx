84161a0d9893f9c249fdbeeb4e02371504ff2f3f
aarondvc <adv69240@uga.edu>
2025-10-07 17:04:51 -0400

    Trying that again

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { GitHubPrivate } from "./pages/GitHubPrivate";
 import { Designs } from "./pages/Designs";
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
